ba8fb7537a2d00a65e1491f9541e9bc894441984
supi06 <47301271+supi06@users.noreply.github.com>
2020-10-26 18:37:24 +0100

    Declare object detector class.

---
+++ detector.py
@@ -0,0 +1,10 @@
+#######################################################################
+#   Class: Detector (detector.py)
+#
+#   Author: Supper Balint
+#   Date: 2020.10.26
+#
+#   This class contains the Convulotional Neural Network, which is 
+#   trained to classie surgical related objects. The class purpose,
+#   is to provide object detection for the object tracker class.
+#######################################################################
