bef7f3acb3d73caf72791f9b709a203b6fef4fbf
supi06 <47301271+supi06@users.noreply.github.com>
2020-10-26 18:39:48 +0100

    Typo correction.

--- detector.py
+++ detector.py
@@ -5,6 +5,6 @@
 #   Date: 2020.10.26
 #
 #   This class contains the Convulotional Neural Network, which is 
-#   trained to classie surgical related objects. The class purpose,
+#   trained to classifie surgical related objects. The class purpose,
 #   is to provide object detection for the object tracker class.
 #######################################################################
