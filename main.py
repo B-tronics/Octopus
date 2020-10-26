59395ae63181115cfbf60d231b8aed434841e724
supi06 <47301271+supi06@users.noreply.github.com>
2020-10-26 18:08:28 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+#######################################################################
+#   File: main.py
+#
+#   Author: Supper Balint
+#   Date: 2020.10.26
+#
+#   Main file which controls all the all the objects instanciated.
+#   Entry point is defined in this file.
+#######################################################################
+
+def main():
+    return 0
+
+if __name__=='__main__':
+    main()
