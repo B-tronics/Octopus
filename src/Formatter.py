8307a44b5c8f487f830374f422aa3f9d79e714cc
supi06 <47301271+supi06@users.noreply.github.com>
2020-10-27 21:04:12 +0100

    Create base logging classes.

---
+++ src/Formatter.py
@@ -0,0 +1,10 @@
+#######################################################################
+#   Class: Formatter (Formatter.py)
+#
+#   Author: Supper Balint
+#   Date: 2020.10.27
+#
+#   This is where you specify the format of the output by specifying a
+#   string format that lists out the attributes that the output should
+#   contain.
+#######################################################################
---
+++ src/Logger.py
@@ -0,0 +1,9 @@
+#######################################################################
+#   Class: Logger (Logger.py)
+#
+#   Author: Supper Balint
+#   Date: 2020.10.27
+#
+#   This is the class whose objects will be used in the application 
+#   code directly to call the functions.
+#######################################################################
