a63b52d76a1345f2ed68a2d49aaae5a0207bc77a
Supper Bálint <47301271+B-tronics@users.noreply.github.com>
2020-12-09 16:38:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -14,24 +14,8 @@ import logging
 # instantiate logger
 log = logging.getLogger(__name__)
 
-
-from src import Logger as log
-
-
 def main():
-<<<<<<< HEAD
-    logger = log.Logger(__name__)
-    logger.logFileNameSetter = 'output'
-
-    try:
-        logger.startLogging(log.logging.DEBUG)
-    except ValueError:
-        print(ValueError)
     return 0
 
-
-=======
-
->>>>>>> logging
 if __name__ == '__main__':
     main()
